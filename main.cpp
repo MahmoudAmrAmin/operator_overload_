5e5afd7f1deee821a70ce826c5fcbd79769c26b7
Mahmoud amr <113695259+mahmod5000@users.noreply.github.com>
2023-10-01 17:30:11 +0300

    commit after bitwise operator

--- main.cpp
+++ main.cpp
@@ -13,39 +13,85 @@ public:
 
 
 //Default Constructor
+
     OverLoad();
+
 // Parametrized constructor
+
     OverLoad(int number1 ,int number2);
+
 // Arithmetic Operators
 
 // " + " function
+
     OverLoad operator + (OverLoad ob ) ;
+
 // " - " function
+
     OverLoad  operator - (OverLoad ob);
+
 // " / " function
+
     OverLoad operator / (OverLoad ob);
+
 // " * " function
+
     OverLoad operator * ( OverLoad ob);
+
 // " % " function
+
     OverLoad operator % ( OverLoad ob );
+
+    // bitwise operator
+
+// " & " operator
+
+    OverLoad operator & (OverLoad ob );
+// " | " operator
+
+    OverLoad operator | (OverLoad ob);
+
+//" ^ " operator
+    OverLoad operator ^ ( OverLoad ob);
+
+// " ~ " operator
+
+    OverLoad operator ~ ();
+
+// " >> " operator
+
+    OverLoad operator >> (OverLoad ob);
+
+// " << " operator
+
+    OverLoad operator << (OverLoad ob);
+
 };
 //Default Constructor
+
     OverLoad ::OverLoad():number1(0) , number2(0){};
+
 //Parametrized constructor
- OverLoad::OverLoad(int number1 ,int number2) {
+
+ OverLoad::OverLoad(int number1 ,int number2)
+{
     this -> number1 = number1 ;
     this -> number2 = number2 ;
 }
 
 // Arithmetic Operators
+
     // " + " function
+
 OverLoad OverLoad::operator+(OverLoad ob) {
         OverLoad temp(0,0);
         temp.number1= number1 + ob.number1 ;
         temp.number2= number2 + ob.number2 ;
         return temp;
 }
+
     // " - " function
+
 OverLoad OverLoad:: operator - (OverLoad ob)
 {
     OverLoad temp(0 , 0);
@@ -53,7 +99,9 @@ OverLoad OverLoad:: operator - (OverLoad ob)
     temp.number2 = number2 - ob. number2;
     return temp;
 }
+
     // " / " function
+
 OverLoad OverLoad ::operator / (OverLoad ob)
 {
     OverLoad temp;
@@ -61,7 +109,9 @@ OverLoad OverLoad ::operator / (OverLoad ob)
     temp.number2 = number2 / ob.number2 ;
     return temp;
 }
+
     // " * " function
+
 OverLoad OverLoad::operator * (OverLoad ob)
 {
         OverLoad temp;
@@ -69,7 +119,9 @@ OverLoad OverLoad::operator * (OverLoad ob)
         temp.number2 = number2 * ob.number2;
         return temp;
 }
+
     // " % " function
+
 OverLoad OverLoad ::operator % (OverLoad ob)
 {
         OverLoad temp;
@@ -80,10 +132,69 @@ OverLoad OverLoad ::operator % (OverLoad ob)
 
 // bitwise operator
 
+    // " & " operator
+
+OverLoad OverLoad ::operator & (OverLoad ob)
+{
+     OverLoad temp ;
+     temp.number1 = number1 & ob.number1;
+     temp.number2 = number2 & ob .number2 ;
+     return temp;
+}
+
+    // " | " operator
+
+OverLoad OverLoad ::operator | (OverLoad ob)
+{
+     OverLoad temp ;
+     temp .number2 = number2 | ob .number2;
+     temp .number1 = number1 | ob.number1;
+     return temp;
+}
+
+    // " ^ " operator
+
+OverLoad OverLoad ::operator ^ (OverLoad ob)
+{
+    OverLoad temp ;
+    temp .number1 = number1 ^ ob. number1;
+    temp. number2 = number2 ^ ob. number2;
+    return temp;
+}
+
+    // " ~ " operator
+
+OverLoad OverLoad ::operator ~ ()
+{
+     OverLoad temp;
+     temp . number1 = ~number1;
+     temp .number2 = ~ number2 ;
+     return temp ;
+}
+
+    // " >> " operator
+OverLoad OverLoad::operator >> (OverLoad ob)
+{
+     OverLoad temp;
+     temp . number1 = number1 >> ob.number1 ;
+     temp . number2 = number2 >> ob .number2 ;
+     return temp;
+}
+
+     // " << " operator
+
+OverLoad OverLoad::operator << (OverLoad ob)
+     {
+         OverLoad temp;
+         temp . number1 = number1 << ob.number1 ;
+         temp . number2 = number2 << ob .number2 ;
+         return temp;
+     }
+
 int main()
 {
-    OverLoad ob1(50, 200) , ob2 ( 4 , 9) , ob3;
-    ob3 = ob1 % ob2 ;
+    OverLoad ob1(2, 4) , ob2 ( 3 , 5) , ob3;
+    ob3 = ob1 << ob2;
     ob3 .print();
 
 
